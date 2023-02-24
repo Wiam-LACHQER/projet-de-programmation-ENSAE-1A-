31b20db4a52d127aeb0d9fdd1c01a77b659414c9
John Doe <wiamlachqer123@gmail.com>
2023-02-24 18:18:15 +0100

    qst 3

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,13 +1,13 @@
 from graph import Graph, graph_from_file
 
 
-data_path = "input/"
+data_path = "projet-de-programmation-ENSAE-1A-/input/"
 file_name = "network.01.in"
 
 g = graph_from_file(data_path + file_name)
 print(g)
 
-"""g.add_edge("Paris", "Palaiseau", 4, 20)
+g.add_edge("Paris", "Palaiseau", 4, 20)
 print(g) # affichage du graphe
 print(g.connected_components_set())
 
